0383542224fc7b1d08203de2aa74d796a86e10b8
kehindeBankole <bankolek1@gmail.com>
2020-09-01 10:14:51 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <SuperHeroState>
-      <Home />
+     <Home />
     </SuperHeroState >
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import UserList from '../component/UserList'
-import Map from '../component/Map'
+
 function Home() {
     return (
-        <div>
-            <UserList/>
-            <Map/>
+        <div className="bg-primary py-5">
+          
         </div>
     )
 }
